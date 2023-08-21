96ed4b48499ec4825cc23404fb9af8df073c4281
gmm2204 <gmm2204@cumc.columbia.edu>
2023-08-21 09:19:36 +0300

    UI Updates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/morlig-angular-template'),
+      dir: require('path').join(__dirname, './coverage/dmi-integrated-dashboards-template'),
       subdir: '.',
       reporters: [
         { type: 'html' },

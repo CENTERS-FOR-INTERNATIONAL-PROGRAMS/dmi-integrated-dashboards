9ce392640c9ddf64e536e1ba3bc857e49e5e976b
George Miller <133229709+gmm2204@users.noreply.github.com>
2023-09-11 10:45:55 +0300

    Merge with upstream (#68)
    * Merge with upstream
    
    * Merge with upstream

--- src/app/models/IDFacility.model.ts
+++ src/app/models/IDFacility.model.ts
@@ -3,7 +3,9 @@ export class IDFacility {
     facility_code: string = '-1';
     facility_name: string = '';
 
-    constructor(facility: IDFacility) {
-        Object.assign(this, facility);
+    constructor(facility_id: number, facility_code: string, facility_name: string) {
+        this.facility_id = facility_id;
+        this.facility_code = facility_code;
+        this.facility_name = facility_name;
     }
 }
